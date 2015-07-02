bf19dbdb9bd55bf181801ba0e25002ed62f35c04
patito <benatto@gmail.com>
2015-07-02 17:22:18 +0100

    Refactoring

--- linked-list/list.py
+++ linked-list/list.py
@@ -15,39 +15,29 @@ class LinkedList(object):
     def __init__(self):
         self.head = None
 
-    def _add_begin(self, name):
+    def append(self, name):
         new = Node(name)
         new.next = self.head
         self.head = new
 
-    def _add_end(self, name):
+    def prepend(self, name):
+        new = Node(name)
         if (not self.head):
-            new = Node(name)
             self.head = new
         else:
             current = self.head
             while (current):
                 previous = current
                 current = current.next
-
-            new = Node(name)
             previous.next = new
 
-    def add(self, name, first):
-        if (first):
-            self._add_begin(name)
-        else:
-            self._add_end(name)
-
     def print_list(self):
-        
         current = self.head
         while (current):
             print current.name
             current = current.next
 
     def size(self):
-
         length = 0
         current = self.head
         while (current):
@@ -58,25 +48,21 @@ class LinkedList(object):
 
     def search(self, name):
         current = self.head
-        found = False
-        pos = 0
-        while (current and found == False):
+        while (current):
             if (current.name == name):
-                found = True
+                return True
             current = current.next
-            pos += 1
 
-        return found, pos
+        return False
 
 
 if __name__ == '__main__':
 
     l = LinkedList()
-    l.add("Paulo", False)
-    l.add("Leonardo", False)
-    l.add("Benatto", False)
-    l.add("Mr.", True)
+    l.prepend("Paulo")
+    l.prepend("Leonardo")
+    l.prepend("Benatto")
+    l.append("Mr.")
     print('Size List = %d' % l.size())
-    found, pos = l.search("Benatto")
-    print('Looking for Benatto found %d at %d' % (found, pos))
+    print('Looking for Benatto = %d' % l.search("Benatto"))
     l.print_list()
