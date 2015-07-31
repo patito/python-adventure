a7b7dee8e175e5fab095c752045480f5bcfc4fb1
patito <benatto@gmail.com>
2015-07-31 23:21:29 +0100

    function to remove vowels

--- eat.py
+++ eat.py
@@ -13,7 +13,13 @@ def eat2(str):
     msg = [s for s in str if s not in 'aeiou']
     return ''.join(msg)
 
+def eat3(str):
+    return ''.join(s for s in str if s not in 'aeiou')
+
 s = eat("Paulo Leonardo Benatto")
 s2 = eat2("Paulo Leonardo Benatto")
+s3 = eat3("Paulo Leonardo Benatto")
+
 print s
 print s2
+print s3
